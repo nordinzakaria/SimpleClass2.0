b1c5ee1b1c261333ebf712e6fabaee7b22352d51
nordinzakaria <nordinzakaria@gmail.com>
2024-10-19 22:33:06 +0800

    Update Date.cs

--- Date.cs
+++ Date.cs
@@ -10,14 +10,6 @@ public class Date
     private int month;
     private int year;
 
-    public Date() { }
-    public Date(int day, int month, int year)
-    {
-        this.day = day;
-        this.month = month;
-        this.year = year;
-    }
-
     public int GetDay() { return day; }
     public int GetMonth() { return month; }
     public int GetYear() { return year; }
